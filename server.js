e9c4bd1831bddee2c194bb5a56118f3dce1cf981
andrislh <andrislhelle@gmail.com>
2026-03-14 21:57:08 +0100

    Fix CORS

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const { PDFDocument, rgb, StandardFonts } = require("pdf-lib");
 
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 
 // Stripe webhook trenger raw body – må komme FØR express.json()
 app.use("/webhook", express.raw({ type: "application/json" }));
